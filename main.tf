88c766b1bff11242f4eab147168ceada81dfca1e
Zohaib Butt <zbutt@muvaki.com>
2019-07-15 12:47:24 -0400

    synatx support for 0.12

--- main.tf
+++ main.tf
@@ -1,24 +1,235 @@
 data "google_client_config" "default" {}
 
 locals {
-    # network related settings
-    region      = "${lookup(var.network, "region", "us-east4")}"
-    network     = "projects/${var.project}/global/networks/${lookup(var.network, "vpc")}"
-    subnetwork  = "projects/${var.project}/regions/${local.region}/subnetworks/${lookup(var.network, "subnetwork")}"
-    tags        = ["${concat(split(",",lookup(var.network, "tags", "")), list(format("%s-gke", var.name)))}"]
-    cluster_secondary_range_name = "${lookup(var.network, "cluster_secondary_range")}"
-    services_secondary_range_name = "${lookup(var.network, "services_secondary_range")}"
-    # Master common settings
-    min_master_version          = "${lookup(var.master_config, "min_master_version")}"
-    maintenance_window          = "${lookup(var.master_config, "maintenance_window_start_time", "03:00")}"
+    project-id = "${var.project == null ? var.project : data.google_client_config.default.project}"
+    workload_identity_default = "${local.project-id}.svc.id.goog"
+}
 
-    # Addons to enable/disable
-    horizontal_pod_autoscaling  = "${lookup(var.addons_config, "disable_horizontal_pod_autoscaling", "false")}"
-    http_load_balancing         = "${lookup(var.addons_config, "disable_http_load_balancing", "false")}"
-    kubernetes_dashboard        = "${lookup(var.addons_config, "disable_kubernetes_dashboard", "true")}"
-    network_policy              = "${lookup(var.addons_config, "enable_network_policy_config", "true")}"
-    tpu_enabled                 = "${lookup(var.addons_config, "enable_tpu", "false")}"
-    # cluster_autoscaling         = "${lookup(var.addons_config, "enable_cluster_autoscaling", "true")}"
-    pod_security_policy         = "${lookup(var.addons_config, "enable_pod_security_policy", "false")}"
+/*
+*       MASTER GKE
+*
+*     - Works only with Regions
+*     - Removes default Node Pool
+*/
 
+data "google_container_engine_versions" "default" {
+    project        = "${local.project-id}"
+    location       = "${var.location}"
+    version_prefix = "${var.gke_version}"
+}
+
+resource "google_container_cluster" "default" {
+    project     = "${local.project-id}"
+    
+    name        = "${var.name}"
+    description = "${var.description}"
+    location    = "${var.location}"
+
+    resource_labels = "${var.labels}"
+
+    # all node pools should be managed independently
+    remove_default_node_pool = "true"
+    # define network and subnetwork
+    network             = "${var.vpc}"
+    subnetwork          = "${var.subnet}"
+
+    # Enforcing new dashboard
+    logging_service     = "${var.logging_service}"
+    monitoring_service  = "${var.monitoring_service}"
+
+    addons_config {
+        horizontal_pod_autoscaling {
+            disabled = "${var.horizontal_pod_autoscaling}"
+        }
+        http_load_balancing {
+            disabled = "${var.http_load_balancing}"
+        }
+        kubernetes_dashboard {
+            disabled = "${var.kubernetes_dashboard}"
+        }
+        network_policy_config {
+            disabled = "${var.network_policy == null ? true : false}"
+        }
+
+        dynamic "istio_config" {
+            for_each = var.istio == null ? [] : list(var.istio)
+
+            content {
+                disabled = istio_config.value.disabled
+                auth     = istio_config.value.auth
+            }
+        }
+
+        cloudrun_config {
+            disabled = "${var.istio != null ? var.istio.disabled == false ? var.cloudrun : true : true}"
+        }
+    }
+
+    dynamic "network_policy" {
+        for_each = var.network_policy == null ? [] : list(var.network_policy)
+
+        content {
+            provider = network_policy.value.provider
+            enabled  = network_policy.value.enabled
+        }
+    }
+
+    cluster_ipv4_cidr = "${var.cluster_ipv4_cidr }"
+    
+    dynamic "cluster_autoscaling" {
+        for_each = var.cluster_autoscaling == null ? [] : [{
+            enabled = true
+            resources = var.cluster_autoscaling
+        }]
+
+        content {
+            enabled = cluster_autoscaling.value.enabled
+            dynamic "resource_limits" {
+                for_each = cluster_autoscaling.value.resources
+
+                content {
+                    resource_type = resource_limits.value.type
+                    minimum       = resource_limits.value.min
+                    maximum       = resource_limits.value.max
+                }
+            }
+        }
+    }
+
+    dynamic "database_encryption" {
+        for_each = var.database_encryption == null ? [] : list(var.database_encryption)
+
+        content {
+            state    = database_encryption.value.state
+            key_name = database_encryption.value.key_name
+        }
+    }
+
+    default_max_pods_per_node   = "${var.default_max_pods_per_node}"
+    enable_binary_authorization = "${var.enable_binary_authorization}"
+    enable_tpu                  = "${var.enable_tpu}"
+
+    pod_security_policy_config {
+        enabled = "${var.pod_security_policy}"
+    }
+
+    # currently only support pre-created secondary ranges
+    dynamic "ip_allocation_policy" {
+        for_each = var.ip_allocation_policy == null ? [] : list(var.ip_allocation_policy)
+
+        content {
+            cluster_secondary_range_name    = ip_allocation_policy.value.cluster_secondary_range_name
+            services_secondary_range_name   = ip_allocation_policy.value.services_secondary_range_name
+        }
+    }
+
+    # Hard Coding this block to disable basic auth / cert creation
+    master_auth {
+        password = ""
+        username = ""
+        client_certificate_config {
+            issue_client_certificate = false
+        }
+    }
+
+    dynamic "master_authorized_networks_config" {
+        for_each = var.master_authorized_networks == null ? [] : var.master_authorized_networks
+
+        content {
+            cidr_blocks {
+                cidr_block   = master_authorized_networks_config.value.cidr_block
+                display_name = master_authorized_networks_config.value.display_name
+            }
+        }
+    }
+
+    min_master_version = "${data.google_container_engine_versions.default.latest_master_version}"
+
+    maintenance_policy {
+        daily_maintenance_window {
+            start_time = "${var.maintenance_window}"
+        }
+    }
+
+    dynamic "authenticator_groups_config" {
+        for_each = var.security_group == null ? [] : list({
+            group = var.security_group
+        })
+
+        content {
+            security_group = authenticator_groups_config.value.group
+        }
+    }
+
+    dynamic "private_cluster_config" {
+        for_each = var.private_cluster_config == null ? [] : list(var.private_cluster_config)
+
+        content {
+            enable_private_endpoint = private_cluster_config.value.enable_private_endpoint
+            enable_private_nodes    = private_cluster_config.value.enable_private_nodes
+            master_ipv4_cidr_block  = private_cluster_config.value.master_ipv4_cidr_block
+        }
+    }
+
+    dynamic "resource_usage_export_config" {
+        for_each = var.bq_usage == null ? [] : list(var.bq_usage)
+
+        content {
+            enable_network_egress_metering = resource_usage_export_config.value.egress_metering
+            bigquery_destination {
+                dataset_id = resource_usage_export_config.value.dataset_id
+            }
+        }
+    }
+
+    vertical_pod_autoscaling {
+        enabled = "${var.vertical_pod_autoscaling}"
+    }
+
+    enable_intranode_visibility = "${var.enable_intranode_visibility}"
+
+    # workload_identity_config = "${var.enable_workload_identity ? local.workload_identity_default : null}"
+
+    # Since we remove default-pool we need to ignore any lifecycle changes to it
+    lifecycle {
+        ignore_changes = ["node_pool"]
+    }
+
+    node_pool {
+        name = "default-pool"
+    }
+}
+
+/*
+*       GKE NODE POOLS
+*/
+resource "google_container_node_pool" "pool" {
+  count       = "${var.node_pools == null ? 0 : length(var.node_pools)}"
+
+  location    = "${var.location}"
+  project     = "${local.project-id}"
+  cluster     = "${google_container_cluster.default.name}"
+
+  name        = "${var.node_pools[count.index].name}"
+
+  initial_node_count = "${var.node_pools[count.index].min_node_per_zone}"
+  max_pods_per_node = "${var.node_pools[count.index].max_pods_per_node}"
+
+  node_config {
+    machine_type    = "${var.node_pools[count.index].machine_type}"
+    labels          = "${var.node_pools[count.index].labels}"
+    service_account = "${var.node_pools[count.index].service_account}"
+    tags            = "${var.node_pools[count.index].tags}"
+  }
+
+  autoscaling {
+    min_node_count = "${var.node_pools[count.index].min_node_per_zone}"
+    max_node_count = "${var.node_pools[count.index].max_node_per_zone}" 
+  }
+
+    # hardcoding gke goodies for now
+    management {
+        auto_repair   = true
+        auto_upgrade  = true
+    }
 }
--- variable.tf
+++ variable.tf
@@ -1,5 +1,6 @@
+# General Configs
 variable "module_dependency" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "This is a dummy value to create module dependency"
 }
@@ -12,19 +13,19 @@ variable "name" {
 variable "project" {
     description = "The project in which the resource belongs. If it is not provided, the provider project is used."
     type        = "string"
-    default     = ""
+    default     = null
 }
 
 variable "service_account" {
     description = "Service Account to use with cluster"
-    type        = "string"
-    default     = ""
+    type        = string
+    default     = null
 }
 
 variable "description" {
     description = "Cluster description"
-    type        = "string"
-    default     = "Provisioned by Terraform"
+    type        = string
+    default     = "GKE Cluster - Provisioned by Terraform"
 }
 
 variable "labels" {
@@ -33,42 +34,211 @@ variable "labels" {
     default     = {}
 }
 
-variable "network" {
-    description = "Map of network settings"
-    type        = "map"
-    default     = {}
+variable "gke_version" {
+    description = "Give GKE Version"
+    type        = "string"
 }
 
-variable "master_config" {
-    description = "Common Master Node Configs"
-    type        = "map"
-    default     = {}
+# container specific
+
+variable "location" {
+    description = "The location (region or zone) in which the cluster master will be created, as well as the default node location. If you specify a zone (such as us-central1-a), the cluster will be a zonal cluster with a single cluster master. If you specify a region (such as us-west1), the cluster will be a regional cluster with multiple masters spread across zones in the region, and with default node locations in those zones as well."
+    type        = string
 }
 
-variable "addons_config" {
-    description = "Addon config settings"
-    type        = "map"
-    default     = {}
+# addon config
+variable "horizontal_pod_autoscaling" {
+    description = "The status of the Horizontal Pod Autoscaling addon, which increases or decreases the number of replica pods a replication controller has based on the resource usage of the existing pods. It ensures that a Heapster pod is running in the cluster, which is also used by the Cloud Monitoring service."
+    type        = bool
+    default     = false
 }
 
+variable "http_load_balancing" {
+    description = "The status of the HTTP (L7) load balancing controller addon, which makes it easy to set up HTTP load balancers for services in a cluster. "
+    type        = bool
+    default     = false
+}
+
+variable "kubernetes_dashboard" {
+    description = "The status of the Kubernetes Dashboard add-on, which controls whether the Kubernetes Dashboard is enabled for this cluster."
+    type        = bool
+    default     = true
+}
+
+variable "istio" {
+    description = "Enabling GKE supported ISTIO"
+    type        = object({
+        disabled = bool
+        auth     = string
+    })
+    default = null
+}
+
+variable "cloudrun" {
+    description = "Enabling cloudrun workloads on GKE Cluster. It requires istio config to be enabled"
+    type        = bool
+    default     = true
+}
+
+# other configs
+variable "cluster_ipv4_cidr" {
+    description = "The IP address range of the kubernetes pods in this cluster. Default is an automatically assigned CIDR."
+    type        = string
+    default     = null
+}
+
+variable "cluster_autoscaling" {
+    description = "Per-cluster configuration of Node Auto-Provisioning with Cluster Autoscaler to automatically adjust the size of the cluster and create/delete node pools based on the current needs of the cluster's workload. Description at https://cloud.google.com/kubernetes-engine/docs/how-to/node-auto-provisioning"
+    type        = list(object({
+        type = string
+        min  = number
+        max  = number
+    }))
+    default = null
+}
+
+variable "database_encryption" {
+    description = "Uses KMS Key to encrypt ETCD"
+    type        = object({
+        state    = string
+        key_name = number
+    })
+    default = null
+}
+
+variable "default_max_pods_per_node" {
+    description = "The default maximum number of pods per node in this cluster. Note that this does not work on node pools which are route-based - that is, node pools belonging to clusters that do not have IP Aliasing enabled. See the official documentation for more information."
+    type        = number
+    default     = null
+}
+
+variable "enable_binary_authorization" {
+    description = "Enable Binary Authorization for this cluster. If enabled, all container images will be validated by Google Binary Authorization."
+    type        = bool
+    default     = false
+}
+
+variable "enable_tpu" {
+    description = "Whether to enable Cloud TPU resources in this cluster."
+    type        = bool
+    default     = false
+}
+
+variable "ip_allocation_policy" {
+    description = "Passing values to this block enables VPC Native and IP Alias features"
+    type        = object({
+        cluster_secondary_range_name  = string
+        services_secondary_range_name = string
+    })
+    default = null
+}
+
+variable "logging_service" {
+    description = "The logging service that the cluster should write logs to. Available options include logging.googleapis.com, logging.googleapis.com/kubernetes, and none"
+    type        = string
+    default     = "logging.googleapis.com/kubernetes"
+}
+
+variable "monitoring_service" {
+    description = "The monitoring service that the cluster should write metrics to. Automatically send metrics from pods in the cluster to the Google Cloud Monitoring API. VM metrics will be collected by Google Compute Engine regardless of this setting Available options include monitoring.googleapis.com, monitoring.googleapis.com/kubernetes, and none"
+    type        = string
+    default     = "monitoring.googleapis.com/kubernetes"
+}
+
+variable "master_authorized_networks" {
+    description = "The desired configuration options for master authorized networks. Takes cidr and display name and maps it to authorized access to master list"
+    type        = list(object({
+        cidr_block   = string
+        display_name = string
+    }))
+    default = null
+}
+
+variable "vpc" {
+    description = "The name or self_link of the Google Compute Engine network to which the cluster is connected. For Shared VPC, set this to the self link of the shared network."
+    type        = string
+}
+
+variable "subnet" {
+    description = "The name or self_link of the Google Compute Engine subnetwork in which the cluster's instances are launched."
+    type        = string
+}
+
+variable "network_policy" {
+  description = " Configuration options for the NetworkPolicy feature."
+  type        = object({
+      provider = string
+      enabled  = bool
+  })
+  default = null
+}
+
+variable "pod_security_policy" {
+    description = "Enable the PodSecurityPolicy controller for this cluster. If enabled, pods must be valid under a PodSecurityPolicy to be created."
+    type        = bool
+    default     = false
+}
+
+variable "security_group" {
+    description = "The name of the RBAC security group for use with Google security groups in Kubernetes RBAC. Group name must be in format gke-security-groups@yourdomain.com."
+    type        = string
+    default     = null
+}
+
+variable "private_cluster_config" {
+    description = "A set of options for creating a private cluster. Structure is documented below."
+    type        = object({
+        enable_private_endpoint = bool
+        enable_private_nodes    = bool
+        master_ipv4_cidr_block  = string
+    })
+    default = null
+}
+
+variable "bq_usage" {
+    description = "Enable usage charts to bigquery"
+    type        = object({
+        egress_metering = bool
+        dataset_id      = string
+    })
+    default = null
+}
+
+variable "vertical_pod_autoscaling" {
+    description = "Enable veritcal pod autoscaling support"
+    type        = bool
+    default     = true
+}
+
+variable "enable_workload_identity" {
+    description = "enable workload identity. https://cloud.google.com/kubernetes-engine/docs/how-to/workload-identity"
+    type        = bool
+    default     = false
+}
+
+variable "enable_intranode_visibility" {
+    description = "Whether Intra-node visibility is enabled for this cluster. This makes same node pod to pod traffic visible for VPC network."
+    type        = bool
+    default     = false
+}
+
+variable "maintenance_window" {
+    description = "Time window specified for daily maintenance operations. Specify start_time in RFC3339 format HH:MM, where HH : [00-23] and MM : [00-59] GMT."
+    type        = string
+    default     = "03:00"
+}
+
+# Node Pools
 variable "node_pools" {
-    description = "GKE Node Pool configuration"
-    type        = "list"
-    default     = []
-}
-
-variable "node_pool_labels" {
-    description = "Map of labels for Node Pool. This has to be defined (even if empty) for each nodepool set"
-    type        = "map"
-    default     = {
-        default = {}
-    }
-}
-
-variable "node_pool_tags" {
-    description = "Map of tags for Node Pool. This has to be defined (even if empty) for each nodepool se"
-    type        = "map"
-    default     = {
-        default = []
-    }
+    description = "the information for node pool"
+    type        = list(object({
+        name              = string
+        machine_type      = string
+        labels            = map(string)
+        service_account   = string
+        tags              = list(string)
+        max_pods_per_node = number
+        min_node_per_zone = number
+        max_node_per_zone = number
+    }))
 }
